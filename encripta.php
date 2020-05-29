67a697ec5522182b874544f52eeee60d6ea57afc
Kike Algarra <kike@areavisual.net>
2020-05-29 18:06:11 +0200

    Uso de openssl_encrypt y openssl_decrypt
    Uso de openssl_encrypt y openssl_decrypt.

--- encripta.php
+++ encripta.php
@@ -10,8 +10,33 @@ $dato_encriptado = $encriptar($dato);
 //Desencripta información:
 $dato_desencriptado = $desencriptar($dato_encriptado);
 
+echo "Entrada de dato: ". $dato . '<br>';
 echo 'Dato encriptado: '. $dato_encriptado . '<br>';
 echo 'Dato desencriptado: '. $dato_desencriptado . '<br>';
 
 echo "IV generado: " . $getIV();
+
+
+
+
+// To encrypt a string
+
+$dataToEncrypt = 'Hello World';
+
+$cypherMethod = 'AES-256-CBC';
+$key = random_bytes(32);
+$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($cypherMethod));
+
+$encryptedData = openssl_encrypt($dataToEncrypt, $cypherMethod, $key, $options=0, $iv);
+
+echo '<br>' . "Entrada de dato: " . $dataToEncrypt  . '<br>';
+
+echo 'Dato encriptado: '. $encryptedData . '<br>';
+// To decrypt an encrypted string
+
+$decryptedData = openssl_decrypt($encryptedData, $cypherMethod, $key, $options=0, $iv);
+
+echo 'Dato desencriptado: '. $decryptedData . '<br>';
+
+
 ?>
